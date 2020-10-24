fae036ae47ebd4d18207f29633ae5b1898ccf63d
chinkonhi <luffychen1202@gmail.com>
2020-10-24 11:26:56 +0900

    listButton.vueのlinter修正

--- components/atoms/listButton.vue
+++ components/atoms/listButton.vue
@@ -1,28 +1,28 @@
 <template>
   <a href="#">
-    <button :class="$style.listButton" >いちらん</button>
+    <button :class="$style.listButton">いちらん</button>
   </a>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
+import Vue from 'vue'
 
-  export default Vue.extend({
-    name: 'ListButton',
-    data() {
-      return {}
-    },
-  })
+export default Vue.extend({
+  name: 'ListButton',
+  data() {
+    return {}
+  },
+})
 </script>
 
 <style module lang="scss">
-  .listButton{
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    background-color: #929d49;
-    color: #f3ebd8;
-    font-size: 18px;
-    border: none;
-  }
+.listButton{
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #929d49;
+  color: #f3ebd8;
+  font-size: 18px;
+  border: none;
+}
 </style>
