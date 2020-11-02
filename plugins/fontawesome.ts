68c8e593b752d4278dc820baf325e4646a85dded
ChiekoIshii <chi24601@gmail.com>
2020-11-02 17:45:27 +0900

    addButton.vue追加

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 config.autoAddCss = false
 
-const icons = [faCheckCircle]
+const icons = [faCheckCircle, faPlus]
 
 // 利用するアイコンをlibraryに追加
 icons.forEach((icon) => library.add(icon))
