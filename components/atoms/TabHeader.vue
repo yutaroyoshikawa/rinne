74123dfe710608ed1959e27f7b9dc2aeec5032d4
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-11-03 19:42:24 +0900

    TabHeaderの作成

---
+++ components/atoms/TabHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :class="$style.container">
+    <h2 :class="$style.header">
+      {{ title }}
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+type Data = {
+  firstTitle: string
+  secondTitle: string
+  currentPosition: 'first' | 'second' | null
+}
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: 'タブ',
+    },
+  },
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  width: 100%;
+  height: 50px;
+  padding: 0 20px;
+  background-color: #f6c521;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.header {
+  text-align: center;
+  font-size: 24px;
+  color: #f3ebd8;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+}
+</style>
