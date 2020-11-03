8b1bda573cb76a1a7854443f5a948f26f749af0e
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-11-03 21:33:47 +0900

    スタイル修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,6 +20,19 @@ export default Vue.extend({
 })
 </script>
 
+<style>
+html,
+body,
+#__nuxt,
+#__layout {
+  height: 100%;
+}
+
+body {
+  background-color: #f3ebd8;
+}
+</style>
+
 <style lang="scss" module>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
@@ -31,6 +44,41 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+}
+
+.app {
+  perspective: 2000px;
+  background-color: #555;
+  height: 100%;
+}
+
+.mainContainer {
+  min-height: 100%;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+  background-color: #f3ebd8;
+  transition: filter 300ms, transform 300ms;
+}
+
+.tabContainer {
+  width: 100%;
+  height: 83vh;
+  background-color: #fff;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+  position: fixed;
+  bottom: 0;
+  border-radius: 10px 10px 0 0;
+  overflow: hidden;
+  z-index: 102;
+}
+
+.openTab {
+  filter: blur(4px);
+  transform: scale3d(0.9, 0.9, 0.9) rotate3d(1, 0, 0, 0);
+}
+
+.portal {
+  height: 100%;
+  width: 100%;
 }
 
 .header {
