a1e74da93d9fe3032daa1f83abd77c204752c9c5
chinkonhi <luffychen1202@gmail.com>
2020-11-04 11:53:06 +0900

    プロファイルテキストコンポーネント作成＆フォントファミリー導入

---
+++ components/atoms/profileText.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="$style.profileText">
+    <p :class="$style.username">{{ userName }}のおともだち</p>
+    <p :class="$style.dollname">{{ dollName }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ProfileText',
+  data() {
+    return {
+      userName: 'さくらちゃん',
+      dollName: 'レニー',
+    }
+  },
+})
+</script>
+
+<style module lang="scss">
+.profileText {
+  margin: 0 auto;
+}
+
+.username {
+  text-align: center;
+  color: #3c230d;
+  font-size: 14px;
+  font-family: 'Kosugi Maru', sans-serif;
+}
+
+.dollname {
+  text-align: center;
+  color: #3c230d;
+  font-size: 20px;
+  font-family: 'Kosugi Maru', sans-serif;
+}
+</style>
--- pages/profile.vue
+++ pages/profile.vue
@@ -2,14 +2,19 @@
   <div>
     <div>プロフィールページ</div>
     <NuxtLink to="cleanliness">清潔度ページ</NuxtLink>
+    <ProfileText />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { CHANGE_HEADER_TITLE } from '@/store/index'
+import ProfileText from '@/components/atoms/profileText.vue'
 
 export default Vue.extend({
+  components: {
+    ProfileText,
+  },
   beforeCreate() {
     this.$store.dispatch(CHANGE_HEADER_TITLE, 'プロフィール')
   },
