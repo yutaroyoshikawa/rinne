19a46dab766f11ff2983e18bb72c03a0e7e20101
chinkonhi <luffychen1202@gmail.com>
2020-11-04 12:10:26 +0900

    linter修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,12 +12,12 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-		  {
-		    rel: 'stylesheet',
-		    href:
-		      'https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap',
-		  },
-	  ],
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
