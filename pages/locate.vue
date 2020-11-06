26c4bbb276a14f27dd90f0abdd358bfaa4fca3c9
ChiekoIshii <chi24601@gmail.com>
2020-11-06 18:02:15 +0900

    現在地の取得

--- pages/locate.vue
+++ pages/locate.vue
@@ -1,5 +1,6 @@
 <template>
   <div :class="$style.wrap">
+    <p>lng:{{ lng }}/lat:{{ lat }}</p>
     <div ref="map" :class="$style.map"></div>
   </div>
 </template>
@@ -13,15 +14,28 @@ export default Vue.extend({
   data() {
     return {
       map: {},
+      lat: 35.689607,
+      lng: 139.700571,
     }
   },
   mounted() {
+    this.isAllowedToGeolocation()
     this.createMap()
   },
   beforeCreate() {
     this.$store.dispatch(CHANGE_HEADER_TITLE, 'いばしょ')
   },
   methods: {
+    isAllowedToGeolocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const data = position.coords
+          this.lat = data.latitude
+          this.lng = data.longitude
+          // alert(`lat${this.lat}/lng${this.lng}`)
+        })
+      }
+    },
     createMap() {
       const mapRef = this.$refs.map as HTMLDivElement | undefined
       const mapboxAccessToken = process.env.MAPBOX_ACCESS_TOKEN
@@ -32,7 +46,7 @@ export default Vue.extend({
         this.map = new mapboxgl.Map({
           container: mapRef,
           style: 'mapbox://styles/chi24601/ckh5sf0y600l419pdxff45jj8',
-          center: [139.745451, 35.658577],
+          center: [this.lng, this.lat],
           zoom: 15,
         })
       }
