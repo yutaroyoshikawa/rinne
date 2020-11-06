46d63d3a119488a67f01f94762a38768b16decd7
鈴木里奈 <ths70225@kashiwa-2.local>
2020-11-06 12:53:06 +0900

    ぬいぐるみ(画像)円コンポーネント作成

---
+++ components/atoms/characterImageCircle.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="$style.wrap">
+    <img :class="$style.img" src="/img/characterImage.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'CharacterImageCircle',
+})
+</script>
+<style module lang="scss">
+.wrap {
+  width: 225px;
+  height: 225px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > p {
+    font-size: 14px;
+    color: #3c230d;
+  }
+}
+.img {
+  width: 100%;
+  height: 100%;
+}
+</style>
