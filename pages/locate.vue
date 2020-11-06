3bfa6cb25c1dd97da7163cfe315eb3280dc4035b
ChiekoIshii <chi24601@gmail.com>
2020-11-06 14:05:44 +0900

    日本語に直した

--- pages/locate.vue
+++ pages/locate.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    <div ref="map"></div>
-    <!-- <p>token→{{ token }}</p> -->
+  <div :class="$style.wrap">
+    <div ref="map" :class="$style.map"></div>
   </div>
 </template>
 
@@ -26,16 +25,15 @@ export default Vue.extend({
     createMap() {
       const mapRef = this.$refs.map as HTMLDivElement | undefined
       const mapboxAccessToken = process.env.MAPBOX_ACCESS_TOKEN
-      console.log(mapboxAccessToken)
       if (mapboxAccessToken) {
         mapboxgl.accessToken = mapboxAccessToken
       }
       if (mapRef) {
         this.map = new mapboxgl.Map({
           container: mapRef,
-          style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-          center: [143.767125, 38.681236], // starting position [lng, lat]
-          zoom: 5, // starting zoom
+          style: 'mapbox://styles/chi24601/ckh5sf0y600l419pdxff45jj8',
+          center: [139.745451, 35.658577],
+          zoom: 15,
         })
       }
     },
@@ -44,8 +42,8 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" module>
-#map {
-  width: 100%;
-  height: 100px;
+.map {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
