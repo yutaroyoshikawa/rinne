070996b26ddef798ebc0281dede8694d2d4b2694
ChiekoIshii <chi24601@gmail.com>
2020-11-07 23:55:23 +0900

    現在地にマーカーをつける

--- pages/locate.vue
+++ pages/locate.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="$style.wrap">
-    <p>lng:{{ lng }}/lat:{{ lat }}</p>
+    <p v-if="hasGettedGeoLocation">lng:{{ lng }}/lat:{{ lat }}</p>
     <div ref="map" :class="$style.map"></div>
   </div>
 </template>
@@ -13,62 +13,30 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 
 type Data = {
   map?: mapboxgl.Map
-  lat: number
-  lng: number
-  dollMarker: {
-    type: string
-    features: {
-      type: string
-      geometry: {
-        type: string
-        coordinates: [number, number]
-      }
-      properties: {
-        title: string
-        description: string
-      }
-    }[]
-  }
+  lat?: number
+  lng?: number
 }
 
 export default Vue.extend({
   data(): Data {
     return {
       map: undefined,
-      lng: 139.65031059999998,
-      lat: 35.6761919,
-      dollMarker: {
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [139.6605246, 35.579322999999995],
-            },
-            properties: {
-              title: 'Mapbox',
-              description: 'Washington, D.C.',
-            },
-          },
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [139.65031059999998, 35.6761919],
-            },
-            properties: {
-              title: 'Mapbox',
-              description: 'San Francisco, California',
-            },
-          },
-        ],
-      },
+      lng: undefined,
+      lat: undefined,
     }
+  },
+  computed: {
+    hasGettedGeoLocation(): boolean {
+      const existLocation =
+        typeof this.lat === 'number' && typeof this.lng === 'number'
+      if (existLocation) {
+        this.createMap()
+      }
+      return existLocation
+    },
   },
   mounted() {
     this.isAllowedToGeolocation()
-    this.createMap()
   },
   beforeCreate() {
     this.$store.dispatch(CHANGE_HEADER_TITLE, 'いばしょ')
@@ -90,24 +58,22 @@ export default Vue.extend({
       if (mapboxAccessToken) {
         mapboxgl.accessToken = mapboxAccessToken
       }
-      if (mapRef) {
+      if (
+        mapRef &&
+        typeof this.lat === 'number' &&
+        typeof this.lng === 'number'
+      ) {
         const map = new mapboxgl.Map({
           container: mapRef,
           style: 'mapbox://styles/chi24601/ckh5sf0y600l419pdxff45jj8',
           center: [this.lng, this.lat],
-          zoom: 13,
+          zoom: 15,
         })
-        // add markers to map
-        this.dollMarker.features.forEach(function (marker) {
-          // create a HTML element for each feature
-          const el = document.createElement('div')
-          el.className = 'marker'
 
-          // make a marker for each feature and add to the map
-          new mapboxgl.Marker(el)
-            .setLngLat(marker.geometry.coordinates)
-            .addTo(map)
-        })
+        const el = document.createElement('div')
+        el.className = 'marker'
+
+        new mapboxgl.Marker(el).setLngLat([this.lng, this.lat]).addTo(map)
         this.map = map
       }
     },
