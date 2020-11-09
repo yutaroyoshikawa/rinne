608bcfe1a3161fa14f5d7942cbd6b0e5d8f014a0
Releafa <sharpedgeage@gamil.com>
2020-11-09 14:03:44 +0900

    コンポーネントの作成 pageへの実装は現環境の.prettierrcに"endofLine"のautoやpackage.jsonに変更を加えないと行けなくて ProfileButtonのみにしました。

---
+++ components/atoms/ProfileButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <a>
+    <button :class="$style.profilebutton">おともだち</button>
+  </a>
+</template>
+<script lang="ts">
+export default {
+  name: 'ProfileButton',
+  data() {
+    return {}
+  },
+}
+</script>
+<style>
+.profilebutton {
+  width: 200px; /* 横 */
+  height: 40px; /* 高さ */
+  border: none;
+  border-radius: 30px; /* 丸み */
+  background-color: #929d49;
+  font-size: 14px;
+  color: #f3ebd8;
+  text-align: center;
+  margin: 0 auto;
+  display: block;
+}
+</style>
