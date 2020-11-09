86301c5836c19d03f070d95f50e535753d018f55
ChiekoIshii <chi24601@gmail.com>
2020-11-09 15:43:14 +0900

    actionModal追加

---
+++ components/molecule/actionModal.vue
@@ -0,0 +1,104 @@
+<template>
+  <div v-if="showActionModal" :class="$style.modalMask" @click="$emit('close')">
+    <div :class="$style.modalWrapper">
+      <div :class="$style.modalContainer">
+        <div :class="$style.modalTitle">
+          <p>{{ modalTitle }}</p>
+        </div>
+        <div :class="$style.textWrap">
+          <slot />
+        </div>
+        <div :class="$style.buttonCenter">
+          <button :class="$style.modalCancelButton" @click="$emit('close')">
+            Cancel
+          </button>
+          <button :class="$style.modalButton" @click="$emit('action')">
+            OK
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+type Data = {
+  title: String
+}
+export default {
+  name: 'ActionModal',
+  props: {
+    modalTitle: { type: String, default: 'modalTitle' },
+    showActionModal: { type: Boolean, default: false },
+  },
+  data(): Data {
+    return {
+      title: 'modalTitle',
+    }
+  },
+}
+</script>
+<style lang="scss" module>
+@import '@/assets/scss/variables.scss';
+.modalMask {
+  position: fixed;
+  z-index: $modal-zindex;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+}
+.modalWrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+.modalContainer {
+  width: 70%;
+  margin: 0 auto;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+}
+.modalTitle {
+  width: 100%;
+  height: 30px;
+  background-color: $primary-color;
+  display: table;
+  text-align: center;
+  & > p {
+    color: $base-color;
+    display: table-cell;
+    vertical-align: middle;
+    line-height: normal;
+  }
+}
+.textWrap {
+  width: 80%;
+  margin: 0 auto;
+  padding: 30px 0;
+  overflow-wrap: break-word;
+}
+.modalButton {
+  width: 100px;
+  padding: 10px 0;
+  border-radius: 30px;
+  margin: 0 auto 20px auto;
+  background-color: $secondary-color;
+  text-align: center;
+  color: #fff;
+}
+.modalCancelButton {
+  width: 98px;
+  padding: 10px 0;
+  border: solid 1px $dark-base-color;
+  border-radius: 30px;
+  margin: 0 auto 20px auto;
+  background-color: #fff;
+  text-align: center;
+  color: $dark-base-color;
+}
+.buttonCenter {
+  text-align: center;
+}
+</style>
