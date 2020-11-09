2cee78f4b0df1a650a7d0c0a5a1252dc40cf1cb1
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-11-09 23:50:47 +0900

    ARにローディング追加

---
+++ components/organisms/ArAlbum.vue
@@ -0,0 +1,152 @@
+<template>
+  <client-only>
+    <a-scene
+      presenar
+      xrextras-gesture-detector
+      xrextras-almost-there
+      xrextras-runtime-error
+      renderer="colorManagement: true"
+      xrweb="disableWorldTracking: true"
+    >
+      <a-assets>
+        <img
+          v-for="(imageSrc, index) in imageSrcs"
+          :id="`renny${index}`"
+          :key="`renny${index}`"
+          :src="imageSrc"
+        />
+      </a-assets>
+      <a-camera
+        position="0 4 10"
+        raycaster="objects: .cantap"
+        cursor="fuse: false; rayOrigin: mouse;"
+      >
+      </a-camera>
+
+      <a-light type="directional" intensity="0.5" position="1 1 1"></a-light>
+
+      <a-light type="ambient" intensity="0.7"></a-light>
+
+      <a-entity xrextras-named-image-target="name: renny">
+        <template v-if="isFoundXrimage">
+          <a-image
+            v-for="(imageSrc, index) in imageSrcs"
+            :key="imageSrc"
+            class="cantap"
+            name="rennyImage"
+            :src="`#renny${index}`"
+            scale="0.0001 0.0001 0.0001"
+            :animation="{
+              property: 'scale',
+              to: '0.9 0.9 0.9',
+              easing: 'easeOutElastic',
+              dur: 3000,
+              delay: 300 * index - 1,
+            }"
+            :animation__2="{
+              property: 'position',
+              to: `${index - 1} ${isTapImage ? 0.3 : 0} 0.3`,
+              easing: 'easeOutElastic',
+              dur: 3000,
+              delay: 300 * index,
+            }"
+            @click="onClickImage(imageSrc)"
+          />
+        </template>
+      </a-entity>
+    </a-scene>
+  </client-only>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapState } from 'vuex'
+
+Vue.config.ignoredElements = [
+  'a-scene',
+  'a-entity',
+  'a-camera',
+  'a-box',
+  'a-ring',
+  'a-asset-items',
+  'a-assets',
+  'a-cursor',
+  'a-text',
+  'a-light',
+  'a-image',
+]
+
+type Data = {
+  isFoundXrimage: boolean
+  isTapImage: boolean
+  images: string[]
+}
+
+export default Vue.extend({
+  name: 'ArAlbum',
+  data(): Data {
+    return {
+      isFoundXrimage: false,
+      isTapImage: false,
+      images: ['renny', 'renny2', 'renny3'],
+    }
+  },
+  computed: {
+    ...mapState(['photoStore/imageSrcs']),
+  },
+  beforeDestroy() {
+    const XR8 = window.XR8
+    if (XR8) {
+      if (!XR8.isPaused()) {
+        XR8.pause()
+      }
+    }
+  },
+  mounted() {
+    const XR8 = window.XR8
+    if (XR8) {
+      if (XR8.isPaused()) {
+        XR8.resume()
+      }
+    }
+    this.initAframe()
+  },
+  methods: {
+    onClickImage(imageName: string) {
+      alert(imageName)
+      this.isTapImage = true
+    },
+    initAframe() {
+      const AFRAME = window.AFRAME
+      if (AFRAME) {
+        const onXrimagefound: (ctx: any) => void = () => {
+          this.isFoundXrimage = true
+        }
+        const onXrimagelost: (ctx: any) => void = () => {
+          this.isFoundXrimage = false
+        }
+        const onRealityReady: (event: any) => void = (event) => {
+          this.$emit('reality-ready', event)
+        }
+        const onRealityError: (error: any) => void = (error) => {
+          this.$emit('reality-error', error)
+        }
+        AFRAME.registerComponent('presenar', {
+          init() {
+            this.el.sceneEl.addEventListener('xrimagefound', onXrimagefound)
+            this.el.sceneEl.addEventListener('xrimagelost', onXrimagelost)
+            this.el.sceneEl.addEventListener('realityready', onRealityReady)
+            this.el.sceneEl.addEventListener('realityerror', onRealityError)
+          },
+          remove() {
+            this.el.sceneEl.removeEventListener('xrimagefound', onXrimagefound)
+            this.el.sceneEl.removeEventListener('xrimagelost', onXrimagelost)
+            this.el.sceneEl.removeEventListener('realityready', onRealityReady)
+            this.el.sceneEL.removeEventListener('realityerror', onRealityError)
+          },
+        })
+      }
+    },
+  },
+})
+</script>
