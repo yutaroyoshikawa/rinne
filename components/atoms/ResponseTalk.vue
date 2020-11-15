a5617c3a020a4008a0e149cb15e6e5ef5bf643bc
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-11-15 13:34:08 +0900

    responseの追加

---
+++ components/atoms/ResponseTalk.vue
@@ -0,0 +1,80 @@
+<template>
+  <transition
+    :enter-class="$style.scaleEnter"
+    :leave-to-class="$style.scaleLeaveTo"
+    :enter-active-class="$style.scaleEnterActive"
+    :leave-active-class="$style.scaleLeaveActive"
+  >
+    <div
+      v-if="talkResponseText || isLoadingTalkResponseText"
+      :class="$style.responseTalk"
+    >
+      <div v-if="!isLoadingTalkResponseText">{{ talkResponseText }}</div>
+      <div v-else>かんがえ中...</div>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapState } from 'vuex'
+
+export default Vue.extend({
+  name: 'ResponseTalk',
+  computed: {
+    ...mapState('ar', ['isLoadingTalkResponseText', 'talkResponseText']),
+  },
+})
+</script>
+
+<style lang="scss" module>
+@import '@/assets/scss/variables.scss';
+
+.responseTalk {
+  width: 250px;
+  height: 50px;
+  border-radius: 30px;
+  background-color: $base-color;
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -15px;
+    border: 15px solid transparent;
+    border-top: 15px solid $base-color;
+  }
+}
+
+@keyframes bounceIn {
+  from {
+    transform: scale(0);
+  }
+  to {
+    transform: scale(1);
+  }
+}
+
+@keyframes bounceOut {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(0);
+  }
+}
+
+.scaleEnterActive {
+  animation: bounceIn 600ms cubic-bezier(0.89, -0.11, 0.07, 1.4);
+}
+
+.scaleLeaveActive {
+  animation: bounceOut 600ms cubic-bezier(1, -0.46, 0.065, 1.005);
+}
+</style>
--- store/ar.ts
+++ store/ar.ts
@@ -1,16 +1,26 @@
-import { MutationTree } from 'vuex'
+import { MutationTree, ActionTree } from 'vuex'
+import { RootState } from './'
 
 type ArStore = {
   isLoadedPresentationAframe: boolean
   isLoadedIndexAframe: boolean
+  talkResponseText?: String
+  isLoadingTalkResponseText: boolean
 }
 
 export const LOADEDND_PRESENTATION_AFRAME = 'LOADEDND_PRESENTATION_AFRAME'
 export const LOADEDND_INDEX_AFRAME = 'LOADEDND_INDEX_AFRAME'
+export const SET_TALK_RESPONSE = 'SET_TALK_RESPONSE'
+export const SET_IS_LOADING_TALK_RESPONSE_TEXT =
+  'SET_IS_LOADING_TALK_RESPONSE_TEXT'
+
+export const REQUEST_TALK_TEXT = 'REQUEST_TALK_TEXT'
 
 export const state = (): ArStore => ({
   isLoadedIndexAframe: false,
   isLoadedPresentationAframe: false,
+  talkResponseText: undefined,
+  isLoadingTalkResponseText: false,
 })
 
 export const mutations: MutationTree<ArStore> = {
@@ -19,5 +29,30 @@ export const mutations: MutationTree<ArStore> = {
   },
   [LOADEDND_INDEX_AFRAME](state) {
     state.isLoadedIndexAframe = true
+  },
+  [SET_TALK_RESPONSE](state, payload: ArStore['talkResponseText']) {
+    state.talkResponseText = payload
+  },
+  [SET_IS_LOADING_TALK_RESPONSE_TEXT](
+    state,
+    payload: ArStore['isLoadingTalkResponseText']
+  ) {
+    state.isLoadingTalkResponseText = payload
+  },
+}
+
+export const actions: ActionTree<ArStore, RootState> = {
+  [REQUEST_TALK_TEXT]({ commit }, talkText: ArStore['talkResponseText']) {
+    if (talkText) {
+      commit(SET_IS_LOADING_TALK_RESPONSE_TEXT, true)
+      setTimeout(() => {
+        if (talkText.includes('おはよう')) {
+          commit(SET_TALK_RESPONSE, 'おはよう！！')
+        } else {
+          commit(SET_TALK_RESPONSE, '何言ってるかわからないよ')
+        }
+        commit(SET_IS_LOADING_TALK_RESPONSE_TEXT, false)
+      }, 3000)
+    }
   },
 }
