b1473a4c71fae6182ec57668a062b5a4efc81626
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-11-22 16:13:30 +0900

    AlbamListとしてコンポーネント分離

---
+++ components/atoms/PhotoListImage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="$style.imageWrap">
+    <img :src="$props.src" :alt="$props.name" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'PhotoListImage',
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: 'image',
+    },
+  },
+})
+</script>
+
+<style lang="scss" module>
+@import '@/assets/scss/variables.scss';
+
+.imageWrap {
+  float: left;
+  width: 140px;
+  height: 140px;
+  background: $secondary-color;
+  margin-top: 20px;
+  position: relative;
+
+  & > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
