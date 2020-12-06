ae80537fcc6c8124434e2be7b1a6221cbb0f8477
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-12-06 15:21:42 +0900

    class追加

--- components/organisms/ArAlbum.vue
+++ components/organisms/ArAlbum.vue
@@ -22,7 +22,7 @@
         <div></div>
       </template>
     </OpacityTransition>
-    <div>
+    <div　:class="$style.modalWrap">
       <ActionModal
         :show-action-modal="isOpenDetailsModal"
         modal-title="くわしく"
