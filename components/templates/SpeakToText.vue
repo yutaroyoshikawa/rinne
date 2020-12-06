ab44e3db26d56fc8efc5b610df70587a0ff8e1f3
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-12-06 14:22:21 +0900

    config修正

--- components/templates/SpeakToText.vue
+++ components/templates/SpeakToText.vue
@@ -158,7 +158,11 @@ export default Vue.extend({
             encoding: 'LINEAR16',
             sampleRateHertz: AUDIO_SAMPLE_RATE,
             languageCode: 'ja-JP',
-            model: 'phone_call',
+            model: 'command_and_search',
+            metadata: {
+              recordingDeviceType: 'SMARTPHONE',
+              originalMimeType: 'audio/wav',
+            },
           },
           audio: {
             content: base64Data,
