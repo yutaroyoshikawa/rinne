cde3e9d0bf66ad31fc700cfaa224aacd7e79e46b
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-12-06 18:50:16 +0900

    initialize移動

--- components/templates/TopLayout.vue
+++ components/templates/TopLayout.vue
@@ -36,8 +36,6 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { CHANGE_HEADER_TITLE } from '@/store/index'
-import { ENABLE_PRESEN_MODE, ENABLE_NOMAL_MODE } from '@/store/ar'
 import Loading from '@/components/organisms/loading.vue'
 import IndexMenu from '@/components/molecule/IndexMenu.vue'
 import TalkButton from '@/components/atoms/TalkButton.vue'
@@ -77,23 +75,6 @@ export default Vue.extend({
   },
   computed: {
     ...mapState('ar', ['isLoadedAframe']),
-  },
-  watch: {
-    '$route.path'() {
-      if (this.$route.path === '/') {
-        this.$store.dispatch(CHANGE_HEADER_TITLE, undefined)
-      }
-    },
-  },
-  beforeCreate() {
-    if (typeof window !== 'undefined') {
-      const presenParam = this.$route.query.presen
-      if (presenParam || presenParam === '1') {
-        this.$store.commit(`ar/${ENABLE_PRESEN_MODE}`)
-      } else {
-        this.$store.commit(`ar/${ENABLE_NOMAL_MODE}`)
-      }
-    }
   },
   methods: {
     onRealityReady() {
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,26 @@
 <template>
   <div />
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { CHANGE_HEADER_TITLE } from '@/store/index'
+import { ENABLE_PRESEN_MODE, ENABLE_NOMAL_MODE } from '@/store/ar'
+
+export default Vue.extend({
+  name: 'Top',
+  created() {
+    this.$store.dispatch(CHANGE_HEADER_TITLE, undefined)
+  },
+  beforeCreate() {
+    if (typeof window !== 'undefined') {
+      const presenParam = this.$route.query.presen
+      if (presenParam || presenParam === '1') {
+        this.$store.commit(`ar/${ENABLE_PRESEN_MODE}`)
+      } else {
+        this.$store.commit(`ar/${ENABLE_NOMAL_MODE}`)
+      }
+    }
+  },
+})
+</script>
