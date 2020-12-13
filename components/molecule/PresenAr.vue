3dea0959f4e138e146a13c755ac1593a8e7e0637
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-12-13 23:06:30 +0900

    上に追加

--- components/molecule/PresenAr.vue
+++ components/molecule/PresenAr.vue
@@ -31,25 +31,24 @@
 
         <a-light type="ambient" intensity="0.7"></a-light>
 
-        <a-entity
-          xrextras-named-image-target="name: renny"
-          geometry="primitive: plane; width: 2; height: 0.4"
-          scale="0.0001 0.0001 0.0001"
-          material="shader: html; target: #response; transparent: true; ratio: width; fps: 1.5"
-          position="0 3.5 0"
-          :animation="{
-            property: 'scale',
-            to:
-              (isLoadingTalkResponseText || talkResponseText) && talkMode
-                ? '1 1 1'
-                : '0.0001 0.0001 0.0001',
-            easing: 'easeOutElastic',
-            dur: 5000,
-          }"
-        />
-
         <a-entity xrextras-named-image-target="name: renny">
           <template v-if="isFoundXrimage">
+            <a-entity
+              geometry="primitive: plane; width: 2; height: 0.4"
+              scale="0.0001 0.0001 0.0001"
+              material="shader: html; target: #response; transparent: true; ratio: width; fps: 1.5"
+              position="0 3.5 0"
+              :animation="{
+                property: 'scale',
+                to:
+                  (isLoadingTalkResponseText || talkResponseText) && talkMode
+                    ? '1 1 1'
+                    : '0.0001 0.0001 0.0001',
+                easing: 'easeOutElastic',
+                dur: 5000,
+              }"
+            />
+
             <!-- <a-plane width="1" height="1" material="src:#talkElement"></a-plane> -->
             <a-image
               v-for="(imageSrc, index) in imageSrcs"
