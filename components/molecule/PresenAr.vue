3a5b42c9e9f19184a8248826d167457ce5162ea2
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-12-22 23:58:58 +0900

    フラグ修正

--- components/molecule/PresenAr.vue
+++ components/molecule/PresenAr.vue
@@ -30,49 +30,47 @@
 
       <a-light type="ambient" intensity="0.7"></a-light>
 
-      <template v-if="!isPausedAr">
-        <a-entity xrextras-named-image-target="name: rinne-device">
-          <template v-if="isFoundXrimage">
-            <a-entity
-              geometry="primitive: plane; width: 2; height: 0.4"
-              scale="0.0001 0.0001 0.0001"
-              material="shader: html; target: #response; transparent: true; ratio: width; fps: 1.5"
-              position="0 3 0"
-              :animation="{
-                property: 'scale',
-                to: talkMode ? '1 1 1' : '0.0001 0.0001 0.0001',
-                easing: 'easeOutElastic',
-                dur: 5000,
-              }"
-            />
+      <a-entity xrextras-named-image-target="name: rinne-device">
+        <template v-if="isFoundXrimage">
+          <a-entity
+            geometry="primitive: plane; width: 2; height: 0.4"
+            scale="0.0001 0.0001 0.0001"
+            material="shader: html; target: #response; transparent: true; ratio: width; fps: 1.5"
+            position="0 3 0"
+            :animation="{
+              property: 'scale',
+              to: talkMode ? '1 1 1' : '0.0001 0.0001 0.0001',
+              easing: 'easeOutElastic',
+              dur: 5000,
+            }"
+          />
 
-            <!-- <a-plane width="1" height="1" material="src:#talkElement"></a-plane> -->
-            <a-image
-              v-for="(imageSrc, index) in imageSrcs"
-              :key="imageSrc"
-              class="cantap"
-              name="rennyImage"
-              :src="`#renny${index}`"
-              scale="0.0001 0.0001 0.0001"
-              :animation="{
-                property: 'scale',
-                to: !talkMode ? '0.9 0.9 0.9' : '0.0001 0.0001 0.0001',
-                easing: 'easeOutElastic',
-                dur: 3000,
-                delay: 300 * index - 1,
-              }"
-              :animation__2="{
-                property: 'position',
-                to: !talkMode ? `${index - 1} 0 0.3` : '0.0001 0.0001 0.0001',
-                easing: 'easeOutElastic',
-                dur: 3000,
-                delay: 300 * index,
-              }"
-              @click="$emit('select-image', index)"
-            />
-          </template>
-        </a-entity>
-      </template>
+          <!-- <a-plane width="1" height="1" material="src:#talkElement"></a-plane> -->
+          <a-image
+            v-for="(imageSrc, index) in imageSrcs"
+            :key="imageSrc"
+            class="cantap"
+            name="rennyImage"
+            :src="`#renny${index}`"
+            scale="0.0001 0.0001 0.0001"
+            :animation="{
+              property: 'scale',
+              to: !talkMode ? '0.9 0.9 0.9' : '0.0001 0.0001 0.0001',
+              easing: 'easeOutElastic',
+              dur: 3000,
+              delay: 300 * index - 1,
+            }"
+            :animation__2="{
+              property: 'position',
+              to: !talkMode ? `${index - 1} 0 0.3` : '0.0001 0.0001 0.0001',
+              easing: 'easeOutElastic',
+              dur: 3000,
+              delay: 300 * index,
+            }"
+            @click="$emit('select-image', index)"
+          />
+        </template>
+      </a-entity>
     </a-scene>
   </client-only>
 </template>
